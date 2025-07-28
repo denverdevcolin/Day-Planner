ae3f6906817f90cb14ab34f587e77eeb8f32a16b
Colin Stanford <j.colin.stanford@gmail.com>
2025-07-28 13:12:33 -0600

    html time slots and js variables and innerhtml

---
+++ js/main.js
@@ -0,0 +1,27 @@
+
+// create an array of items that you might want to accomplish on any given day
+
+const array = ["Walk", "Study", "Workout", "Call a loved one", "Hydrate", "Read five pages of something", "Do the dishes", "Prep for dinner", "Shower", "Laundry"];
+
+// create some variables and assign them to each list item
+
+let timeSlot1 = document.getElementById("time-slot1");
+let timeSlot2 = document.getElementById("time-slot2");
+let timeSlot3 = document.getElementById("time-slot3");
+let timeSlot4 = document.getElementById("time-slot4");
+let timeSlot5 = document.getElementById("time-slot5");
+let timeSlot6 = document.getElementById("time-slot6");
+
+// append/add an array item to the innerhtml
+timeSlot1.innerHTML += array[0];
+timeSlot2.innerHTML += array[1];
+timeSlot3.innerHTML += array[2];
+timeSlot4.innerHTML += array[3];
+timeSlot5.innerHTML += array[4];
+// great, now you need to get the for loop to do that
+
+// loop through the array
+for (let i = 0; i < array.length; i++) {
+
+}
+
