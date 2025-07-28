860fa1b4f3b99ca393b839b86b90af2ca3989ca8
Colin Stanford <j.colin.stanford@gmail.com>
2025-07-28 16:43:01 -0600

    array for each time slot plus nested for loops

--- js/main.js
+++ js/main.js
@@ -12,16 +12,22 @@ let timeSlot4 = document.getElementById("time-slot4");
 let timeSlot5 = document.getElementById("time-slot5");
 let timeSlot6 = document.getElementById("time-slot6");
 
+// the time slots will be put into an array so we can loop through that too
+let timeArray = [timeSlot1, timeSlot2, timeSlot3, timeSlot4, timeSlot5, timeSlot6];
+
 // append/add an array item to the innerhtml
-timeSlot1.innerHTML += array[0];
-timeSlot2.innerHTML += array[1];
-timeSlot3.innerHTML += array[2];
-timeSlot4.innerHTML += array[3];
-timeSlot5.innerHTML += array[4];
+// timeSlot1.innerHTML += array[0];
+// timeSlot2.innerHTML += array[1];
+// timeSlot3.innerHTML += array[2];
+// timeSlot4.innerHTML += array[3];
+// timeSlot5.innerHTML += array[4];
+
 // great, now you need to get the for loop to do that
 
 // loop through the array
 for (let i = 0; i < array.length; i++) {
-
+    for (let k = 0; i < timeArray.length; i++) {
+        timeArray[i].innerHTML += array[i];
+    }
 }
 
