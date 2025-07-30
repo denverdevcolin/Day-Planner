f02f7523fd148e66748b4d3c285466583d80e45d
Colin Stanford <j.colin.stanford@gmail.com>
2025-07-30 16:37:44 -0600

    testing duplicate issue

--- js/main.js
+++ js/main.js
@@ -1,15 +1,21 @@
 
-const array = ["Walk", "Study", "Workout", "Call a loved one", "Hydrate", "Read five pages of something", "Do the dishes", "Prep for dinner", "Shower", "Laundry"];
+const array = ["Walk", "Study", "Workout", "Clean one room", "Call a loved one", "Hydrate", "Read five pages of something", "Do the dishes", "Prep for dinner", "Shower", "Laundry"];
 
-// this variable is assigned a number, specifically, a number between 0 and the length of the array, essentially giving me an index
+// declare a variable assign it to a random number, specifically, a number between 0 and the length of the array, essentially giving you an index
 // let i = Math.floor(Math.random() * array.length);
 
 // Appending HTML to an element. Setting the value of innerHTML lets you append new contents to the existing one of an element (mdn, innerHTML docs)
 const list = document.getElementById("list");
-// list.innerHTML += `<li></li>`; // creates one new list item
+// list.innerHTML += `<li></li>`; // creates one new list item on the page
 
-// creates one new list item for each element in the array
+// create one new list item for each element in the array
 for (let i = 0; i < array.length; i++) {
     list.innerHTML += `<li>${array[Math.floor(Math.random() * array.length)]}</li>`;
+
+    // unordered list ends up receiving duplicates. how do we keep them out?
+    // 
+    if () {
+
+    }
 }
 
