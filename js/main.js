0481994f88a05070c80b8b4021a6ce751fd980a4
Colin Stanford <j.colin.stanford@gmail.com>
2025-08-01 13:56:24 -0600

    addeventlistener for input, testing for loop to add string to array

--- js/main.js
+++ js/main.js
@@ -1,18 +1,35 @@
 
-const array = ["Walk", "Study", "Workout", "Clean one room", "Call a loved one", "Hydrate", "Read five pages of something", "Do the dishes", "Prep for dinner", "Shower", "Laundry"];
+// const array = ["Walk", "Study", "Workout", "Clean one room", "Call a loved one", "Hydrate", "Read five pages of something", "Do the dishes", "Prep for dinner", "Shower", "Laundry"];
+
+const btnEl = document.querySelector('button');
+const inputEl = document.querySelector('input');
+const listEl = document.getElementById("list");
+// create an array and add each value with push?
+let array = [];
+
+btnEl.addEventListener('click', () => {
+    // const inputValue = inputEl.value;
+    const inputValue = inputEl.value;
+    inputValue.split(', ');
+    array.push(inputValue);
+    for (let i = 0; i < array.length; i++) {
+        listEl.insertAdjacentHTML('beforeend', `<li>${inputValue}</li>`);
+    }
+    
+});
 
 // declare a variable assign it to a random number, specifically, a number between 0 and the length of the array, essentially giving you an index
 // let i = Math.floor(Math.random() * array.length);
 
 // Appending HTML to an element. Setting the value of innerHTML lets you append new contents to the existing one of an element (mdn, innerHTML docs)
-const list = document.getElementById("list");
+// const list = document.getElementById("list");
 // list.innerHTML += `<li></li>`; // creates one new list item on the page
 
 // create one new list item for each element in the array
-for (let i = 0; i < array.length; i++) {
-    list.innerHTML += `<li>${array[i]}</li>`;
+// for (let i = 0; i < array.length; i++) {
+//     list.innerHTML += `<li>${array[i]}</li>`;
 
     // unordered list ends up receiving duplicates. how do we keep them out?
     // 
-}
+
 
