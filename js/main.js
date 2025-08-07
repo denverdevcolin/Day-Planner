0971084142540c1c641a82eafa8ec63b8c890ee0
Colin Stanford <j.colin.stanford@gmail.com>
2025-08-07 14:35:54 -0600

    duplicates still an issue

--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
 
-// const array = ["Walk", "Study", "Workout", "Clean one room", "Call a loved one", "Hydrate", "Read five pages of something", "Do the dishes", "Prep for dinner", "Shower", "Laundry"];
-
 const btnEl = document.querySelector('button');
 const inputEl = document.querySelector('input');
 const listEl = document.getElementById('list');
+let startHour = 8;
 
 btnEl.addEventListener('click', () => {
 
@@ -11,15 +10,22 @@ btnEl.addEventListener('click', () => {
    
     for (let i = 0; i < inputValue.length; i++) {
 
-        if (inputValue[i] == listEl.innerText) {
-            throw new Error(`${inputValue[i]} is already in your task list!`);
-        } else {
-            listEl.insertAdjacentHTML('beforeend', `<li>${inputValue[i]}</li>`);
-        }
+        let time = `${startHour}:00`;
+        startHour++;
+        
+        listEl.insertAdjacentHTML(
+            'beforeend',
+            `<li>${time} -${inputValue[i]}</li>`
+        );
+
 
     }
 
+    inputEl.value = '';
+
 });
+
+
 
 
 // declare a variable assign it to a random number, specifically, a number between 0 and the length of the array, essentially giving you an index
@@ -28,9 +34,3 @@ btnEl.addEventListener('click', () => {
 // Appending HTML to an element. Setting the value of innerHTML lets you append new contents to the existing one of an element (mdn, innerHTML docs)
 // const list = document.getElementById("list");
 // list.innerHTML += `<li></li>`; // creates one new list item on the page
-
-// create one new list item for each element in the array
-// for (let i = 0; i < array.length; i++) {
-//     list.innerHTML += `<li>${array[i]}</li>`;
-
-
