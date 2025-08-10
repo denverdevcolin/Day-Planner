84e2f7ca975ed6f33a930f636e8af83d7aae6cb3
Colin Stanford <j.colin.stanford@gmail.com>
2025-08-10 15:06:27 -0600

    front end looking better

--- js/main.js
+++ js/main.js
@@ -1,29 +1,78 @@
 
-const btnEl = document.querySelector('button');
+// const btnEl = document.querySelector('button');
+const addBtn = document.getElementById('addTasks');
+const clearBtn = document.getElementById('clearTasks');
+
 const inputEl = document.querySelector('input');
 const listEl = document.getElementById('list');
-let startHour = 8;
 
-btnEl.addEventListener('click', () => {
+const summary = document.getElementById('summary');
+
+let currentHour = 8;
+let allItems = new Set();
+
+clearBtn.addEventListener('click', () => {
+    
+    clearAllTasks();
+});
+
+addBtn.addEventListener('click', () => {
 
     let inputValue = inputEl.value.split(",");
    
-    for (let i = 0; i < inputValue.length; i++) {
-
-        let time = `${startHour}:00`;
-        startHour++;
+    // for (let i = 0; i < inputValue.length; i++) {
+    //     let time = `${currentHour}:00`;
+    //     currentHour++;
         
-        listEl.insertAdjacentHTML(
-            'beforeend',
-            `<li>${time} -${inputValue[i]}</li>`
-        );
+    //     listEl.insertAdjacentHTML(
+    //         'beforeend',
+    //         `<li>${time} -${inputValue[i]}</li>`
+    //     );
+    // }
 
+    handleNewSubmission(inputValue);
 
-    }
+    const taskCount = allItems.size;
+    const nextTime = formatTime(currentHour);
+    summary.innerHTML = `<strong>Current Schedule:</strong>${taskCount} | <strong>Next available time:</strong> ${nextTime}`;
 
     inputEl.value = '';
-
+    
 });
+
+function handleNewSubmission (userInput) {
+
+    const newItems = userInput.filter(item => !allItems.has(item));
+
+    if (newItems.length === 0) {
+        alert("No new items to add!");
+        return;
+    }
+
+    for (let i = 0; i < newItems.length; i++) {
+        allItems.add(newItems[i]);
+        const scheduleItem = document.createElement('div');
+        scheduleItem.textContent = `${currentHour}:00 - ${newItems[i]}`;
+        listEl.appendChild(scheduleItem);
+        currentHour++;
+    }
+
+}
+
+function clearAllTasks () {
+    if (allItems.size === 0) {
+        alert("Task List is already clear!");
+        return;
+    }
+}
+
+
+
+
+
+
+
+
 
 
 
